235441d36a05c9ca076a4a208d475b1b97f5b79f
Shai Halimi <shalimi@c2r8s2.42lausanne.ch>
2022-11-02 19:36:02 +0100

    maj

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: shalimi <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 16:59:28 by shalimi           #+#    #+#             */
-/*   Updated: 2022/11/02 02:20:46 by shalimi          ###   ########.fr       */
+/*   Updated: 2022/11/02 19:12:30 by shalimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -162,7 +162,7 @@ int	handle_here_doc(char *eof)
 #include <stdio.h>
 int	main(int argc, char **argv, char **env)
 {
-	int	pid;
+	int	*pid;
 	int fd[2];
 	int	out[2];
 	int i;
@@ -175,19 +175,21 @@ int	main(int argc, char **argv, char **env)
 	i= 2;
 	
 	paths= ft_split(&(env[ft_find_paths_index(env)][5]), ':');
-	pid = fork();
-	if (!pid && ft_strncmp(argv[1],"here_doc", 8) == 0)
+	if (ft_strncmp(argv[1],"here_doc", 8) == 0)
 	{
 		file1_fd = handle_here_doc(argv[2]);
+		pid = malloc(sizeof(int) * argc - 4);
 		i++;
-		exit(0);
 	}
 	else
+	{
 		file1_fd = open(argv[1], O_RDONLY);
-	
+		pid = malloc(sizeof(int) * argc - 3);
+
+	}
 	pipe(fd);
-	pid = fork();
-	if (!pid)
+	pid[0] = fork();
+	if (!pid[0])
 	{
 		dup2(file1_fd, 0);
 		args = argv[i];
@@ -198,13 +200,14 @@ int	main(int argc, char **argv, char **env)
 	}
 	else
 	{
-	//	close(file1_fd);
+		close(file1_fd);
 		i++;
 		if (argc == 5)
 		{
 			out[0] = fd[0];
 			out[1] = fd[1];
 		}
+		int j = 1;
 		while (i != argc - 2)
 		{
 			if (i > 3)
@@ -213,8 +216,8 @@ int	main(int argc, char **argv, char **env)
 				fd[1] = out[1];
 			}
 			pipe(out);
-			pid = fork();
-			if(!pid)
+			pid[j] = fork();
+			if(!pid[j])
 			{
 				// REGARDE ICI
 				dup2(fd[0], 0);
@@ -233,9 +236,10 @@ int	main(int argc, char **argv, char **env)
 				//close(out[1]);
 			}
 			i++;
+			j++;
 		}
-		pid = fork();
-		if (!pid)
+		pid[j] = fork();
+		if (!pid[j])
 		{
 			dup2(out[0], 0);	
 			file2_fd = open(argv[argc - 1],  O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -249,7 +253,9 @@ int	main(int argc, char **argv, char **env)
 		{
 			close(fd[0]);
 			close(fd[1]);
-			wait(&pid);
+			int m = 0;
+			while (m < j)
+				wait(&pid[m++]);
 		}
 	}
 	return (0);
